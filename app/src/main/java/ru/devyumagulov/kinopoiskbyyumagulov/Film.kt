f2e11a10464dc70996b3a7efd800c40f0c042108
Yumek95 <yumek95@gmail.com>
2021-05-29 18:25:12 +0700

    - теперь данные фильма (постер, название, описание) открываются в новом окне

---
+++ app/src/main/java/ru/devyumagulov/kinopoiskbyyumagulov/Film.kt
@@ -0,0 +1,11 @@
+package ru.devyumagulov.kinopoiskbyyumagulov
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String
+) : Parcelable
