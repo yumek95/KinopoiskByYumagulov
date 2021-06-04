991af552f5c980d60867ab2843538d97f6fb0427
Yumek95 <yumek95@gmail.com>
2021-06-04 23:22:08 +0700

    - добавил логику добавления фильмов в избранное и просмотра спика избранного (приложение крашится при нажатии на кнопку "добавить в ищбранное" в фрагменте с фильмом) - добавил кнопку "поделиться" (приложение крашится при ее нажатии)

--- app/src/main/java/ru/devyumagulov/kinopoiskbyyumagulov/DetailsFragment.kt
+++ app/src/main/java/ru/devyumagulov/kinopoiskbyyumagulov/DetailsFragment.kt
@@ -1,5 +1,6 @@
 package ru.devyumagulov.kinopoiskbyyumagulov
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_details.*
 
 class DetailsFragment : Fragment() {
+    private lateinit var film : Film
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +22,35 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setFilmsDetails()
+
+        //Кнопка "Избранное"
+        details_fab_favorites.setOnClickListener {
+            if (!film.isInFavorites) {
+                details_fab_favorites.setImageResource(R.drawable.ic_round_favorites)
+                film.isInFavorites = true
+            } else {
+                details_fab_favorites.setImageResource(R.drawable.ic_round_favorites_border)
+                film.isInFavorites = false
+            }
+        }
+
+        //Кнопка "Поделиться"
+        details_fab.setOnClickListener {
+            //Создаем интент
+            val intent = Intent()
+            //Указываем action с которым он запускается
+            intent.action = Intent.ACTION_SEND
+            //Кладем данные о нашем фильме
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Зацени этот фильм: ${film.title} \n\n ${film.description}"
+            )
+            //Указываем MIME тип, чтобы система знала, какое приложения предложить
+            intent.type = "text/plain"
+            //Запускаем наше активити
+            startActivity(Intent.createChooser(intent, "Share To:"))
+        }
+
     }
 
     private fun setFilmsDetails() {
@@ -32,5 +63,11 @@ class DetailsFragment : Fragment() {
         details_poster.setImageResource(film.poster)
         //Устанавливаем описание
         details_description.text = film.description
+
+        //Установка нужной иконки при запуске фрагмента
+        details_fab_favorites.setImageResource(
+            if (film.isInFavorites) R.drawable.ic_round_favorites
+            else R.drawable.ic_round_favorites_border
+        )
     }
 }
